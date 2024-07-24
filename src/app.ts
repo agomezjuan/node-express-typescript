55783c600055afa5d3063bec0f994a1dc1216cb1
Alberto Gómez Juan <agomezjuan@hotmail.com>
2024-07-24 11:46:32 +0200

    feat: :sparkles: init server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hola MoFo!!");
+import { Server } from "./presentation/server";
+
+(() => {
+  main();
+})();
+
+async function main() {
+  console.log("Hello world");
+
+  // todo: await for database
+
+  // todo: await for server
+  new Server({ port: 3000 }).start();
+}
